1f8e5007ea898d7c704a0a4e9c3e5393a005a160
greg9702 <grzegorzb9702@gmail.com>
2023-07-24 19:45:03 +0200

    Fixed stack api

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -12,6 +12,7 @@ export function ApiStack({ stack }: StackContext) {
       },
     },
     routes: {
+      "GET /notes/{id}": "packages/functions/src/get.main",
       "GET /notes": "packages/functions/src/list.main",
       "POST /notes": "packages/functions/src/create.main",
       "PUT /notes/{id}": "packages/functions/src/update.main",
