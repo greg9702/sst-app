8a164b38880d9e86edd563b1956b680e9d5980e9
greg9702 <grzegorzb9702@gmail.com>
2023-07-28 17:36:08 +0200

    Added CORS

--- packages/core/src/handler.ts
+++ packages/core/src/handler.ts
@@ -18,6 +18,10 @@ export default function handler(lambda: Function) {
     return {
       statusCode,
       body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   };
 }
