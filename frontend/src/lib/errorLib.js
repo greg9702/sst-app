b8a9e80ea1642bbdadb361d804f604b0e85ee66f
greg9702 <grzegorzb9702@gmail.com>
2023-07-30 14:05:08 +0200

    Added create note views

--- frontend/src/lib/errorLib.js
+++ frontend/src/lib/errorLib.js
@@ -1,6 +1,6 @@
 export function onError(error) {
   let message = error.toString();
-
+  console.log(error.details);
   // Auth errors
   if (!(error instanceof Error) && error.message) {
     message = error.message;
